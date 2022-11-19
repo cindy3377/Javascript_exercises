7638463b26b09f3609e46faee6a9dec8cf6f5c43
cindy3377 <92247502+cindy3377@users.noreply.github.com>
2022-11-19 14:44:19 +0200

    first commit

---
+++ Assignment_2/app2.js
@@ -0,0 +1,4 @@
+document.write("Exercise 2<br><br>")
+
+let user_name = prompt("Type your name:");
+document.write(`Hello, ${user_name}!`);
---
+++ Assignment_5/app5.js
@@ -0,0 +1,10 @@
+document.write("Exercise 5<br><br>")
+
+input_year = parseInt(prompt("Enter a year:"));
+
+if (input_year % 4 === 0 && input_year % 100 !== 0 || input_year % 400 === 0) {
+    document.write("It is a leap year.");
+}
+else {
+    document.write("It is not a leap year.");
+}
